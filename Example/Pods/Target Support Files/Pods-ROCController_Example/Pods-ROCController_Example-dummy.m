45f0538970d6d1e29bcd87da43e2f512b86a15cf
Maximilian Alexander <mbalex99@gmail.com>
2017-03-17 15:39:57 -0700

    removed ignored files

--- Example/Pods/Target Support Files/Pods-ROCController_Example/Pods-ROCController_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ROCController_Example : NSObject
-@end
-@implementation PodsDummy_Pods_ROCController_Example
-@end
--- Example/Pods/Target Support Files/ROCController/ROCController-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ROCController : NSObject
-@end
-@implementation PodsDummy_ROCController
-@end
